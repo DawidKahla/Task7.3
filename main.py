246bd6869024ee061fba6b8799ed4923df718885
DawidKahla <dawid.kahla@onet.pl>
2023-02-20 21:56:53 +0100

    add basic function, method and dynamic atrr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from faker import Faker
 fake = Faker()
-# 'John'
 
 class visiting_card:
     def __init__(self, name, surname, company, position, email):
@@ -11,7 +10,12 @@ class visiting_card:
         self.email = email
     def __str__(self):
         return f"{self.name} {self.surname}, {self.position} in {self.company}, e-mail adress: {self.email}"
+    def contact(self):
+        print(f"Kontaktuję się z {self.name} {self.surname}, {self.position}, {self.email}.")    
+    @property
+    def name_length(self):
+        return len(self.name) + 1 + len(self.surname)
+    
+def make_fake():
+    return visiting_card(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.free_email())
 
-for _ in range(10):
-    card = visiting_card(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.free_email())
-    print(card)
