deef576e2864b296410bf624250d10a0bfaa82da
DawidKahla <dawid.kahla@onet.pl>
2023-02-20 22:08:13 +0100

    divide class to base and business

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from faker import Faker
 fake = Faker()
 
-class visiting_card:
-    def __init__(self, name, surname, company, position, email):
+class BaseContact:
+    def __init__(self, name, surname, phone_number, email):
         self.name = name
         self.surname = surname
-        self.company = company
-        self.position = position
-        self.email = email
-    def __str__(self):
-        return f"{self.name} {self.surname}, {self.position} in {self.company}, e-mail adress: {self.email}"
+        self.phone_number = phone_number
+        self.email = email 
     def contact(self):
-        print(f"Kontaktuję się z {self.name} {self.surname}, {self.position}, {self.email}.")    
+        print(f"Wybieram numer +48 {self.phone_number} i dzwonię do {self.name} {self.surname}.")   
     @property
-    def name_length(self):
+    def label_length(self):
         return len(self.name) + 1 + len(self.surname)
+
+class BusinessContact(BaseContact):
+    def __init__(self, work_number, company, job, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.work_number = work_number
+        self.company = company
+        self.job = job
+    def contact(self):
+        print(f"Wybieram numer +48 {self.work_number} i dzwonię do {self.name} {self.surname}.")   
     
-def make_fake():
-    return visiting_card(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.free_email())
+def create_contacts():
+    pass
+    #return visiting_card(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.free_email())
 
