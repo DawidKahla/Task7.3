ad53da6c7ca81b2315d5b537181fd6db153fdd38
DawidKahla <51680032+DawidKahla@users.noreply.github.com>
2023-02-22 23:00:52 +0100

    add create_contacts body and example of use

--- main.py
+++ main.py
@@ -22,7 +22,22 @@ class BusinessContact(BaseContact):
     def contact(self):
         print(f"Wybieram numer +48 {self.work_number} i dzwonię do {self.name} {self.surname}.")   
     
-def create_contacts():
-    pass
-    #return visiting_card(name=fake.first_name(), surname=fake.last_name(), company=fake.company(), position=fake.job(), email=fake.free_email())
+# returns list of chosen number of random contacts with specified type
+def create_contacts(number_of_contacts, contact_type):
+  list_of_contacts = []
+  for i in range(1,number_of_contacts+1):
+      if contact_type == 'Base':
+          new_contact = BaseContact(name=fake.first_name(), surname=fake.last_name(), phone_number=fake.country_calling_code()+' '+str(randrange(500000000,899999999)), email=fake.free_email())
+      elif contact_type == 'Business':
+          # there is possiblity that phone_number == work_number 
+          new_contact = BusinessContact(name=fake.first_name(), surname=fake.last_name(), phone_number=fake.country_calling_code()+' '+str(randrange(500000000,899999999)), email=fake.free_email(), work_number=fake.country_calling_code()+' '+str(randrange(500000000,899999999)),company=fake.company(),job=fake.job())
+      else:
+        print("Zły typ wizytówki")
+        break
+      list_of_contacts.append(new_contact)
+  return list_of_contacts
 
+# example of use
+contacts_list = create_contacts(72, 'Business')
+for single_contact in contacts_list:
+  single_contact.contact()
